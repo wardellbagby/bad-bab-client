072e59a80b6e8fdf328fdc4bbe1c88ffedb72e98
DJ <djsm00thee3@gmail.com>
2019-05-29 10:36:57 -0700

    Adding arrows to collapsable items

--- src/components/Court.js
+++ src/components/Court.js
@@ -1,20 +1,26 @@
-import React, {useEffect} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import {selectPlayer} from "../actions";
+import React, {useState} from 'react';
 import _ from 'lodash';
 
 export default function Court({court}) {
+    const [paneIsVisible, setPaneVisibility] = useState(true);
 
-    const cardId = "court-" + court.id;
+    const updatePaneVisibility = () => setPaneVisibility(!paneIsVisible);
+
+    const caretDirection = paneIsVisible ? "down" : "left";
+
+    const collapse = paneIsVisible ? "show" : "";
 
     return (
         <div className="form-row card bg-secondary">
-            <a className="card-header" data-toggle="collapse" data-target={"#" + cardId}>
-                <h5 className="mb-0">
-                        Court {court.id}
+            <a className="card-header" onClick={updatePaneVisibility}>
+                <h5 className="mb-0 d-flex">
+                    <span class="flex-grow-1">Court {court.id}</span>
+
+                    <i className={`fas fa-caret-${caretDirection}`}/>
                 </h5>
             </a>
-            <div id={cardId} className="collapse show">
+
+            <div className={`collapse ${collapse}`}>
                 <div className="card-body py-3">
                     {court.reservations.map(reservation => (
                         <div>
