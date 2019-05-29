7de89b0c24913bcf924968e8d0776c6763a1a623
DJ <djsm00thee3@gmail.com>
2019-05-29 14:55:16 -0700

    Making top level cars collapse-able

--- src/containers/Card.js
+++ src/containers/Card.js
@@ -1,21 +1,31 @@
-import React, {useEffect} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import {requestCourts} from "../actions";
+import React, {useState} from 'react';
 import '../style/PlayerSelector.css';
-import Court from "../components/Court";
 import _ from 'lodash';
 
 export default function Card({title, children}) {
+    const [paneIsVisible, setPaneVisibility] = useState(true);
     const className = _.toLower(title) + " bg-dark card btn-group border-secondary";
+
+    const updatePaneVisibility = () => setPaneVisibility(!paneIsVisible);
+
+    const caretDirection = paneIsVisible ? "down" : "left";
+
+    const collapseClass = paneIsVisible ? "collapse show" : "collapse";
 
     return (
         <div className={className}>
-            <div className="card-header bg-secondary text-light">
-                <h5 className="mb-0">{title}</h5>
-            </div>
+            <a className="card-header bg-secondary text-light" onClick={updatePaneVisibility}>
+                <h5 className="mb-0 d-flex">
+                    <span className="flex-grow-1">{title}</span>
 
-            <div className="card-body">
-                {children}
+                    <i className={`fas fa-caret-${caretDirection}`}/>
+                </h5>
+            </a>
+
+            <div className={collapseClass}>
+                <div className="card-body">
+                    {children}
+                </div>
             </div>
         </div>
     );
