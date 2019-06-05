9afdf18d4294ec335fdd39e885e570097b473b10
DJ <djsm00thee3@gmail.com>
2019-06-04 22:41:27 -0700

    Updating the default UI of the main panel. This creates tabs instead of large cards

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,33 @@ import Courts from "../containers/Courts";
 function App() {
   return (
     <div className="App">
-        <SelectedPlayerList />
-        <MemberSelector />
-        <PlayerSelector />
-        <Courts />
+        <ul className="nav nav-pills bg-dark mt-1 justify-content-around" role="tablist">
+            <li className="nav-item">
+                <a className="nav-link active" id="players-pills" data-toggle="pill" href="#players" role="tab"
+                   aria-controls="players" aria-selected="true">Players</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" id="profile-pills" data-toggle="pill" href="#courts" role="tab"
+                   aria-controls="courts" aria-selected="false">Courts</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" id="contact-pills" data-toggle="pill" href="#members" role="tab"
+                   aria-controls="members" aria-selected="false">Members</a>
+            </li>
+        </ul>
+
+        <div className="tab-content">
+            <div className="tab-pane fade show active" id="players" role="tabpanel">
+                <SelectedPlayerList/>
+                <PlayerSelector/>
+            </div>
+            <div className="tab-pane fade" id="courts" role="tabpanel">
+                <Courts/>
+            </div>
+            <div className="tab-pane fade" id="members" role="tabpanel">
+                <MemberSelector/>
+            </div>
+        </div>
     </div>
   );
 }
