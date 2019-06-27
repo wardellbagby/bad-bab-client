53bd30e84e71550b0825b4b54ce41b15c68c4321
DJ <djsm00thee3@gmail.com>
2019-06-27 08:10:02 -0700

    Adding build files, setting up git hub pages for now

---
+++ build/static/js/6.b83085a7.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{900:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",function(){return i});var o=e(9);function i(n){n.addEventListener("statusTap",function(){Object(o.m)(function(){var t=n.innerWidth,e=n.innerHeight,i=n.document.elementFromPoint(t/2,e/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then(function(){Object(o.u)(function(){return c.scrollToTop(300)})})}})})}}}]);
+//# sourceMappingURL=6.b83085a7.chunk.js.map
