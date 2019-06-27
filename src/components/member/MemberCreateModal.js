2fdb6dac3c14ee41db71c48c73c0bc5b860c228b
DJ <djsm00thee3@gmail.com>
2019-06-26 21:51:37 -0700

    Combining member create modal and member password form

--- src/components/member/MemberCreateModal.js
+++ src/components/member/MemberCreateModal.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
-import MemberPasswordForm from "./MemberPasswordForm";
-import {Modal, ModalBody} from '../../components/Modal';
+import React, {useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Modal, ModalBody, ModalFooter} from '../../components/Modal';
+import {IonCardHeader, IonCardTitle} from "@ionic/react";
+import {cancelCreatingMember, createPlayer} from "../../actions";
+import PasswordSelector from "../../containers/PasswordSelector";
 
 export function MemberCreateModal() {
-    const memberToCreate = useSelector(state => state.selected.memberToCreate);
+    const dispatch = useDispatch();
 
-    if (memberToCreate === null) {
-        return <span/>;
-    }
+    const member = useSelector(state => state.selected.memberToCreate);
+    const [memberPassword, updateMemberPassword] = useState("");
+
+    const handleCancel = () => dispatch(cancelCreatingMember());
+    const handlePlayerCreate = () => dispatch(createPlayer({ name: member.name, password: memberPassword }));
 
     return (
         <Modal>
             <ModalBody>
-                <MemberPasswordForm member={memberToCreate}/>
+                <IonCardHeader>
+                    <IonCardTitle>{member.name}</IonCardTitle>
+                </IonCardHeader>
+
+
+                <PasswordSelector selectedPassword={memberPassword}
+                                  updatePassword={updateMemberPassword}/>
+
+                <ModalFooter
+                    onCancel={handleCancel}
+                    onSuccess={handlePlayerCreate}/>
             </ModalBody>
         </Modal>
     );
